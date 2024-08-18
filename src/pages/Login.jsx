c0735d11873a5ece0ca1f832c6d843f270c74eca
Ahmed Korani <ahmed@shire.blue>
2024-08-18 03:08:52 +0300

    Add Login page UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,58 @@
-import * as firebaseui from "firebaseui";
-import "firebaseui/dist/firebaseui.css";
-import { useEffect } from "react";
-import { auth } from "../utils/FirebaseConfig";
+import { auth, provider } from "../utils/FirebaseConfig";
+import { useNavigate } from "react-router-dom";
+import { signInWithPopup } from "firebase/auth";
+import { Box, Typography } from "@mui/material";
+import GoogleButton from "react-google-button";
 
 const Login = () => {
-    useEffect(() => {
-        const uiConfig = {
-            signInSuccessUrl: '/role-selector',
-            signInOptions: [
-                {
-                    provider: 'google.com',
-                    authMethod: 'https://accounts.google.com',
-                    clientId: import.meta.env.VITE_CLIENT_ID,
-                },
-            ],
-            tosUrl: '/terms-of-service',
-            privacyPolicyUrl: '/privacy-policy',
-        };
+  const navigate = useNavigate();
 
-        const ui = firebaseui.auth.AuthUI.getInstance() ?? new firebaseui.auth.AuthUI(auth); ui.start('#firebaseui-auth-container', uiConfig);
-    }, []);
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
+      if (user) navigate("/home");
+    } catch (error) {
+      console.error("Error during sign-in:", error);
+    }
+  };
 
-    return <div id="firebaseui-auth-container"></div>;
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: "50%",
+          height: "50%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow: 3,
+          borderRadius: 2,
+          backgroundColor: "white",
+        }}
+      >
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          Welcome To MAS
+        </Typography>
+        <Typography variant="subtitle1" m="1rem">
+          Please sign in first
+        </Typography>
+        <GoogleButton onClick={handleGoogleSignIn}>
+          Sign in with Google
+        </GoogleButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
