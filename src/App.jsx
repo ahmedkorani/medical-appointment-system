9b48777fc89cc3e483a94db6b334a09d7dcb1aea
Ahmed Korani <ahmed@shire.blue>
2024-08-20 04:48:46 +0300

    Add provider context in app root

--- src/App.jsx
+++ src/App.jsx
@@ -15,45 +15,48 @@ import {
 } from "./routes";
 import { Box, CircularProgress } from "@mui/material";
 import ResponsiveAppBar from "./components/Appbar";
+import { AppProvider } from "./store/AppProvider";
 
 const App = () => {
   return (
-    <Box
-      sx={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <ResponsiveAppBar />
-      <Router>
-        <React.Suspense
-          fallback={
-            <Box
-              sx={{
-                height: "100%",
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <CircularProgress />
-            </Box>
-          }
-        >
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/role-selector" element={<RoleSelector />} />
-            <Route path="/patient-dashboard" element={<PatientDashboard />} />
-            <Route path="/clinic-dashboard" element={<ClinicDashboard />} />
-          </Routes>
-        </React.Suspense>
-      </Router>
-    </Box>
+    <AppProvider>
+      <Box
+        sx={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <ResponsiveAppBar />
+        <Router>
+          <React.Suspense
+            fallback={
+              <Box
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <CircularProgress />
+              </Box>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/role-selector" element={<RoleSelector />} />
+              <Route path="/patient-dashboard" element={<PatientDashboard />} />
+              <Route path="/clinic-dashboard" element={<ClinicDashboard />} />
+            </Routes>
+          </React.Suspense>
+        </Router>
+      </Box>
+    </AppProvider>
   );
 };
 
