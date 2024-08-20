b04933eb2e9a71c48943dc684c854206ef2fe328
Ahmed Korani <ahmed@shire.blue>
2024-08-20 05:02:43 +0300

    Update new routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
+import React from "react";
 
-const Login = React.lazy(() => import('./pages/Login'));
-const RoleSelector = React.lazy(() => import('./pages/RoleSelector'));
-const PatientDashboard = React.lazy(() => import('./pages/PatientDashboard'));
-const ClinicDashboard = React.lazy(() => import('./pages/ClinicDashboard'));
+const Login = React.lazy(() => import("./pages/Login"));
 const Home = React.lazy(() => import("./pages/Home"));
 
-export { Login, RoleSelector, PatientDashboard, ClinicDashboard, Home};
+export { Login, Home };
